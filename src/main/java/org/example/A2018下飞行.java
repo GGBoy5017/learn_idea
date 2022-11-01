339d2a6b53ba34a16a3e65fd8cd0998fb584ca45
高歌 <2609565842@qq.com>
2022-11-01 16:25:01 +0800

    learn_idea

---
+++ src/main/java/org/example/A2018下飞行.java
@@ -0,0 +1,32 @@
+package org.example;
+import java.util.*;
+/**
+ * 名称A2018下飞行
+ * 描述
+ *
+ * @version 1.0
+ * @author:GGBoy
+ * @datatime:2022-10-18 21:25
+ */
+
+
+
+public class A2018下飞行 {
+}
+
+abstract class CState{
+    public int flyMiles;
+    public abstract double travle(int miles,CFrequentFlyer context);
+}
+
+class CBasic extends CState{
+
+    @Override
+    public double travle(int miles,CFrequentFlyer context) {
+        return 0;
+    }
+}
+
+class CFrequentFlyer{
+
+}
